54b48b5932f034bfed437f927d12b8c937b3b063
Hieu Huynh <hieuhuynh93@users.noreply.github.com>
2025-03-26 12:47:45 +0700

    update: config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
+  distDir: "build",
+  reactStrictMode: false,
 };
 
 export default nextConfig;
